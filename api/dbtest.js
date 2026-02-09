901e10445c304259cf6515428a42921ab85f036e
sinxmaya <estdiara@gmail.com>
2026-02-09 09:36:57 +0800

    Update dbtest.js

--- api/dbtest.js
+++ api/dbtest.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       server: process.env.DB_HOST,          
-      port: parseInt(process.env.DB_PORT),  
+      //port: parseInt(process.env.DB_PORT),  
       database: process.env.DB_NAME,
       options: {
         encrypt: true,
